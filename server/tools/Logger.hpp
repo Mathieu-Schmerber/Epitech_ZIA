b190bc75baf01a905e29e1db734ff220a4a23b33
Cyprien Ricque <48893621+Cyprien-Ricque@users.noreply.github.com>
2021-03-05 12:36:54 +0100

    Fix logger

--- server/tools/Logger.hpp
+++ server/tools/Logger.hpp
@@ -100,11 +100,6 @@ namespace logging {
         }
 
         void log(const std::string &message) override {
-            //cout is thread safe, to avoid multiple threads interleaving on one line
-            //though, we make sure to only call the << operator once on std::cout
-            //otherwise the << operators from different threads could interleave
-            //obviously we dont care if flushes interleave
-            //std::lock_guard<std::mutex> lk{lock};
             std::cout << message;
             std::cout.flush();
         }
@@ -113,83 +108,7 @@ namespace logging {
         const std::unordered_map<log_level, std::string, enum_hasher> levels;
     };
 
-    //TODO: add log rolling
-    //logger that writes to file
-    class file_logger : public logger {
-    public:
-        file_logger() = delete;
 
-        explicit file_logger(const logging_config_t &config) : logger(config) {
-            //grab the file name
-            auto name = config.find("file_name");
-            if (name == config.end())
-                throw std::runtime_error("No output file provided to file logger");
-            file_name = name->second;
-
-            //if we specify an interval
-            reopen_interval = std::chrono::seconds(300);
-            auto interval = config.find("reopen_interval");
-            if (interval != config.end()) {
-                try {
-                    reopen_interval = std::chrono::seconds(std::stoul(interval->second));
-                }
-                catch (...) {
-                    throw std::runtime_error(interval->second + " is not a valid reopen interval");
-                }
-            }
-
-            //crack the file open
-            reopen();
-        }
-
-        void log(const std::string &message, const log_level level) override {
-            if (level < LOG_LEVEL_CUTOFF)
-                return;
-            std::string output;
-            output.reserve(message.length() + 64);
-            output.append(uncolored.find(level)->second);
-            output.append(message);
-            output.push_back('\n');
-            log(output);
-        }
-
-        void log(const std::string &message) override {
-            lock.lock();
-            file << message;
-            file.flush();
-            lock.unlock();
-            reopen();
-        }
-
-    protected:
-        void reopen() {
-            //TODO: use CLOCK_MONOTONIC_COARSE
-            //check if it should be closed and reopened
-            auto now = std::chrono::system_clock::now();
-            lock.lock();
-            if (now - last_reopen > reopen_interval) {
-                last_reopen = now;
-                try { file.close(); } catch (...) {}
-                try {
-                    file.open(file_name, std::ofstream::out | std::ofstream::app);
-                    last_reopen = std::chrono::system_clock::now();
-                }
-                catch (std::exception &e) {
-                    try { file.close(); } catch (...) {}
-                    throw e;
-                }
-            }
-            lock.unlock();
-        }
-
-        std::string file_name;
-        std::ofstream file;
-        std::chrono::seconds reopen_interval{};
-        std::chrono::system_clock::time_point last_reopen;
-    };
-
-    //a factory that can create loggers (that derive from 'logger') via function pointers
-    //this way you could make your own logger that sends log messages to who knows where
     using logger_creator = logger *(*)(const logging_config_t &);
 
     class logger_factory {
@@ -198,8 +117,6 @@ namespace logging {
             creators.emplace("", [](const logging_config_t &config) -> logger * { return new logger(config); });
             creators.emplace("std_out",
                              [](const logging_config_t &config) -> logger * { return new std_out_logger(config); });
-            creators.emplace("file",
-                             [](const logging_config_t &config) -> logger * { return new file_logger(config); });
         }
 
         [[nodiscard]] logger *produce(const logging_config_t &config) const {

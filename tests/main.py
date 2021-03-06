ddbdf5abde111399918c673c84f76f029a535593
Alexandre Malaquin <alexandre.malaquin@epitech.eu>
2021-03-06 18:48:40 +0100

    Update python main.py

--- tests/main.py
+++ tests/main.py
@@ -1,7 +1,10 @@
+#!/usr/bin/env python3
+
 import requests
 import subprocess
 import os
 from shutil import copyfile
+from colorama import init, Fore, Back, Style
 
 
 def make_diff(path1, path2, r):
@@ -17,22 +20,23 @@ def make_diff(path1, path2, r):
     return 0
 
 
-def check_status_code(r):
-    if r.status_code != 200:
-        print('Wrong status_code: got: ' + str(r.status_code) + ' but expected: 200')
+def check_status_code(r, expected_code):
+    if r.status_code != expected_code:
+        print(Fore.RED, 'Wrong status_code: got: ' + str(r.status_code) + ' but expected: 200')
         return 1
     return 0
+
 
 def test2():
     test_nbr = 2
     test_name = "post test"
     counter_error = 0
     r = requests.post('http://127.0.0.1', data={'key': '10'})
-    counter_error += check_status_code(r)
+    counter_error += check_status_code(r, 204)
     if counter_error == 0:
-        print(str(test_nbr) + ': ' + 'You pass the test ' + test_name)
+        print(Fore.GREEN, str(test_nbr) + ': ' + 'You pass the test ' + test_name)
     else:
-        print(str(test_nbr) + ': ' + 'There was ' + str(counter_error) + " error(s) in the test " + test_name)
+        print(Fore.RED, str(test_nbr) + ': ' + 'There was ' + str(counter_error) + " error(s) in the test " + test_name)
 
 
 def test3():
@@ -40,11 +44,11 @@ def test3():
     test_nbr = 3
     counter_error = 0
     r = requests.head('http://127.0.0.1/index.html')
-    counter_error += check_status_code(r)
+    counter_error += check_status_code(r, 200)
     if counter_error == 0:
-        print(str(test_nbr) + ': ' + 'You pass the test ' + test_name)
+        print(Fore.GREEN, str(test_nbr) + ': ' + 'You pass the test ' + test_name)
     else:
-        print(str(test_nbr) + ': ' + 'There was ' + str(counter_error) + " error(s) in the test " + test_name)
+        print(Fore.RED, str(test_nbr) + ': ' + 'There was ' + str(counter_error) + " error(s) in the test " + test_name)
 
 
 def test4():
@@ -52,22 +56,35 @@ def test4():
     test_nbr = 4
     counter_error = 0
     r = requests.delete('http://127.0.0.1/indexdel.html')
-    counter_error += check_status_code(r)
+    counter_error += check_status_code(r, 200)
     if counter_error == 0:
-        print(str(test_nbr) + ': ' + 'You pass the test ' + test_name)
+        print(Fore.GREEN, str(test_nbr) + ': ' + 'You pass the test ' + test_name)
     else:
-        print(str(test_nbr) + ': ' + 'There was ' + str(counter_error) + " error(s) in the test " + test_name)
+        print(Fore.RED, str(test_nbr) + ': ' + 'There was ' + str(counter_error) + " error(s) in the test " + test_name)
+
 
 def test5():
     test_name = "options request"
     test_nbr = 5
     counter_error = 0
     r = requests.options('http://127.0.0.1/index.html')
-    counter_error += check_status_code(r)
+    counter_error += check_status_code(r, 200)
     if counter_error == 0:
-        print(str(test_nbr) + ': ' + 'You pass the test ' + test_name)
+        print(Fore.GREEN, str(test_nbr) + ': ' + 'You pass the test ' + test_name)
     else:
-        print(str(test_nbr) + ': ' + 'There was ' + str(counter_error) + " error(s) in the test " + test_name)
+        print(Fore.RED, str(test_nbr) + ': ' + 'There was ' + str(counter_error) + " error(s) in the test " + test_name)
+
+
+def test6():
+    test_name = "get request in https"
+    test_nbr = 6
+    counter_error = 0
+    r = requests.get('https://127.0.0.1/index.html', verify=False)
+    counter_error += check_status_code(r, 200)
+    if counter_error == 0:
+        print(Fore.GREEN, str(test_nbr) + ': ' + 'You pass the test ' + test_name)
+    else:
+        print(Fore.RED, str(test_nbr) + ': ' + 'There was ' + str(counter_error) + " error(s) in the test " + test_name)
 
 
 def reset():
@@ -77,21 +94,28 @@ def reset():
 def request_get_index(path):
     counter_error = 0
     r = requests.get('http://127.0.0.1/index.html')
-    counter_error += check_status_code(r)
+    counter_error += check_status_code(r, 200)
     if os.name != "nt":
         counter_error +=make_diff("./tmp_file_to_diff/file_diff_get_index", path + "index.html", r)
     if counter_error == 0:
-        print('\033[32m' + 'you pass the get test with the index.html')
+        print(Fore.GREEN, 'You pass the get test with the index.html')
     else:
-        print('\033[31m' + 'there was ' + str(counter_error) + " error(s) in the get test with the index.html")
+        print(Fore.RED, 'There was ' + str(counter_error) + " error(s) in the get test with the index.html")
 
     test2()
+    print(Style.RESET_ALL, end="")
     test3()
+    print(Style.RESET_ALL, end="")
     test4()
-    test5()
+    print(Style.RESET_ALL, end="")
+    # test5()
+    # print(Style.RESET_ALL, end="")
+    test6()
+    print(Style.RESET_ALL, end="")
     reset()
 
 
 if __name__ == '__main__':
+    init()
     path = './file/'
     request_get_index(path)
